d605affd6c5071cee564553875b6760c3fba5924
Eteran07 <edgar.teran@correo.unimet.edu.ve>
2025-02-14 23:33:33 -0400

    Se realizaron cambios en el archivo main y se implementó la funcion para iniciar sesion

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('Hola Mundo');
+let sequence = [];
+let playerSequence = [];
+let score = 0;
+let playerName = '';
+let highScores = JSON.parse(localStorage.getItem('highScores')) || {};
+
+const colors = ['red', 'green', 'blue', 'yellow'];
+const startButton = document.getElementById('startButton');
+const gameDiv = document.getElementById('game');
+const menuDiv = document.getElementById('Menu'); 
+const currentScoreDisplay = document.getElementById('currentScore');
+const restartButton = document.getElementById('restartButton');
+const highScoresDisplay = document.getElementById('highScores');
+
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', restartGame);
+
+function startGame() {
+    playerName = document.getElementById('playerName').value; 
+    if (!playerName) {
+        alert('Por favor, ingresa tu nombre.');
+        return;
+    }
+    menuDiv.classList.add('hidden');
+    gameDiv.classList.remove('hidden');
+    score = 0;
+    sequence = [];
+    nextRound();
+}
+
